316936f30adc3ded217bd30e3f2f5fe296b9dfd8
sajithsojan <108297201+sajithsojan@users.noreply.github.com>
2024-09-09 16:53:36 +0530

    Added Some comments on buffered writer and reader

--- Example codes/JavaFileIO/BufferedReaderDemo.java
+++ Example codes/JavaFileIO/BufferedReaderDemo.java
@@ -1,3 +1,27 @@
+/*
+
+This is the most enhanced(better) Reader to read character data from the file. 
+Constructors:
+
+BufferedReaderbr=new BufferedReader(Reader r);
+BufferedReaderbr=new BufferedReader(Reader r,intbuffersize); 
+
+Note:BufferedReadercan not communicate directly with the File it should communicate 
+via some Reader object.
+
+The main advantage of BufferedReader over FileReader is we can read data line by line 
+instead of character by character.
+
+Methods:
+1. int read();
+2. int read(char[] ch);
+3. String readLine(); 
+It attempts to read next line and return it , from the File. if the next line is not 
+available then this method returns null.
+4. void close();
+
+ */
+
 import java.io.*;
 
 public class BufferedReaderDemo {
--- Example codes/JavaFileIO/BufferedWriterProgram.java
+++ Example codes/JavaFileIO/BufferedWriterProgram.java
@@ -1,3 +1,16 @@
+/*
+ 
+BufferedWriter:
+By using BufferedWriter object we can write character data to the file
+
+Constructors:
+BufferedWriterbw=new BufferedWriter(writer w);
+BufferedWriterbw=new BufferedWriter(writer w,intbuffersize); 
+
+Note:BufferedWriter never communicates directly with the file it should communicates 
+via some writer object.
+ */
+
 import java.io.*;
 import java.io.IOException;
 
